8d27b7f83d39362e33e6af702522d04de679d64e
JasperDeasey <60531248+JasperDeasey@users.noreply.github.com>
2021-05-13 12:04:23 -0700

    removed unnecessary code; added title and icon

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "fontsource-roboto";
 import Landing from "./ui/sections/landing/landing";
 import About from "./ui/sections/about/about";
 import Projects from "./ui/sections/projects";
-import Experiences from "./ui/sections/experiences";
 import Heading from "./ui/sections/heading";
 
 function App() {
